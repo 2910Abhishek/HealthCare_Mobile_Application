4ca8c3f90d014cef35435592b0434f01ee6a827d
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2024-07-02 22:05:27 +0530

    minor update

--- aarogya/lib/screens/signUpScreen.dart
+++ aarogya/lib/screens/signUpScreen.dart
@@ -15,117 +15,114 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(height: 35),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        // Handle onTap for your icon button
-                      },
-                      child: Icon(
-                        Icons.arrow_back_sharp,
-                        size: 36,
-                        color: const Color.fromARGB(255, 175, 96, 66),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 35),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          // Handle onTap for your icon button
+                        },
+                        child: Icon(
+                          Icons.arrow_back_sharp,
+                          size: 36,
+                          color: const Color.fromARGB(255, 175, 96, 66),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 30),
-                    Text(
-                      "Let's Get Started",
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      "Create an account to continue",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 35),
-                    TextBox(
-                      text: 'Enter your full name',
-                      icon: Icon(Icons.person),
-                      keyboardType: TextInputType.name,
-                    ),
-                    SizedBox(height: 20),
-                    TextBox(
-                      text: 'Enter your phone number',
-                      icon: Icon(Icons.phone),
-                      keyboardType: TextInputType.phone,
-                    ),
-                    SizedBox(height: 20),
-                    TextBox(
-                      text: 'Enter your email',
-                      icon: Icon(Icons.email),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    SizedBox(height: 20),
-                    TextBox(
-                      text: 'Enter your Password',
-                      icon: Icon(Icons.lock),
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 20),
-                    CustomButton(text: 'Create Account'),
-                    SizedBox(height: 15),
-                    Center(
-                      child: Text(
-                        "OR",
+                      SizedBox(height: 30),
+                      Text(
+                        "Let's Get Started",
                         style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Create an account to continue",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 35),
+                      TextBox(
+                        text: 'Enter your full name',
+                        icon: Icon(Icons.person),
+                        keyboardType: TextInputType.name,
+                      ),
+                      SizedBox(height: 20),
+                      TextBox(
+                        text: 'Enter your phone number',
+                        icon: Icon(Icons.phone),
+                        keyboardType: TextInputType.phone,
+                      ),
+                      SizedBox(height: 20),
+                      TextBox(
+                        text: 'Enter your email',
+                        icon: Icon(Icons.email),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      SizedBox(height: 20),
+                      TextBox(
+                        text: 'Enter your Password',
+                        icon: Icon(Icons.lock),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 20),
+                      CustomButton(text: 'Create Account'),
+                      SizedBox(height: 15),
+                      Center(
+                        child: Text(
+                          "OR",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
+                      SizedBox(height: 15),
+                      Center(
+                        child: CustomButtonForGoogle(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 15),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Have an account? ",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
                     ),
-                    SizedBox(height: 15),
-                    Center(
-                      child: CustomButtonForGoogle(),
-                    ),
-                    SizedBox(height: 20),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/login');
-                      },
-                      child: Container(
-                        color: Colors.white,
-                        padding: EdgeInsets.symmetric(vertical: 15),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Center(
-                              child: Text(
-                                "Have an account? ",
-                                textAlign: TextAlign.center,
-                                style:
-                                    TextStyle(fontSize: 12, color: Colors.grey),
-                              ),
-                            ),
-                            SizedBox(height: 5),
-                            Center(
-                              child: Text(
-                                "Sign In",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ],
-                        ),
+                    SizedBox(width: 5),
+                    Text(
+                      "Sign In",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
