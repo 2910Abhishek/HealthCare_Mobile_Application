a2b8bda170b74f13928dc48cb47c01a8df485dba
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2024-07-11 21:15:47 +0530

    signout button added

--- aarogya/lib/main.dart
+++ aarogya/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:aarogya/screens/profilescreen.dart';
 import 'package:aarogya/screens/signUpScreen.dart';
 import 'package:aarogya/screens/tabsscreen.dart';
 import 'package:aarogya/utils/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -16,23 +17,39 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Aarogya Health App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       routes: {
-        '/': (context) => OnBoardingScreen(),
+        '/onboarding': (context) => OnBoardingScreen(),
         '/login': (context) => const LogInScreen(),
         '/signup': (context) => const SignUpScreen(),
-        '/home': (context) => TabsScreen(),
-        '/profile': (context) => ProfileScreen(),
+        '/home': (context) => const TabsScreen(),
+        '/profile': (context) => const ProfileScreen(),
       },
-      initialRoute: '/', // Set initial route to signup screen
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          if (snapshot.hasData) {
+            return const TabsScreen();
+          }
+          return OnBoardingScreen();
+        },
+      ),
     );
   }
 }
--- aarogya/lib/screens/dashboard.dart
+++ aarogya/lib/screens/dashboard.dart
@@ -1,3 +1,5 @@
+import 'package:aarogya/resources/auth_methods.dart';
+import 'package:aarogya/widgets/customButton.dart';
 import 'package:flutter/material.dart';
 
 class DashBoardScreen extends StatefulWidget {
@@ -8,13 +10,14 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class _DashBoardScreenState extends State<DashBoardScreen> {
+  AuthMethods _authMethods = AuthMethods();
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        "Welcome to DashBoard Screen!",
-        style: TextStyle(fontSize: 24, color: Colors.black),
-      ),
-    );
+        child: CustomButton(
+            text: "Sign Out ",
+            onPressed: () {
+              _authMethods.signOut();
+            }));
   }
 }
