4df6d8ede240f6f3dc8af61038ce47c3eb813a3b
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2024-10-27 09:32:33 +0530

    login.py and .env File Updated

--- backened/login.py
+++ backened/login.py
@@ -409,10 +409,14 @@ from io import BytesIO
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 import json
+import os
+from dotenv import load_dotenv  # Add this import
 
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/Doctor'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)
